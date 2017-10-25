f664188f795002edcd068b28967ceebb6672907e
gnshdas28 <gnshdas28@gmail.com>
2017-10-25 16:20:53 +0000

    Add files via upload

--- face_align_2D.py
+++ face_align_2D.py
@@ -23,45 +23,89 @@ import os
 
 shape_pred="/home/das/opencv/samples/Das/Das_pre_trained/shape_predictor_68_face_landmarks.dat"
 
-source_folder="/home/das/opencv/samples/Das/Das_pre_trained/pics"
-dest_folder="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces/"
+cnn_pred="/home/das/opencv/samples/Das/Das_pre_trained/mmod_human_face_detector.dat"
+
+
+cnn_face_detector = dlib.cnn_face_detection_model_v1(cnn_pred)
+win = dlib.image_window()
+
+
+date = "19-10-17"
+
+source_folder="/home/das/opencv/samples/Das/Das_pre_trained/"+date
+dest_folder="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces"+ "_" +date
+
+
+try:
+    os.stat(dest_folder)
+except:
+    os.mkdir(dest_folder)  
 
 for filename in os.listdir(source_folder):
 	# load the input image, resize it, and convert it to grayscale
 	image = cv2.imread(os.path.join(source_folder,filename))
-	image = imutils.resize(image, width=1000)
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	# initialize dlib's face detector (HOG-based) and then create
-	# the facial landmark predictor and the face aligner
-	detector = dlib.get_frontal_face_detector()
-	predictor = dlib.shape_predictor(shape_pred)
-	fa = FaceAligner(predictor, desiredFaceWidth=152)
+	# image = imutils.resize(image, width=1000)
+	# gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+	# # initialize dlib's face detector (HOG-based) and then create
+	# # the facial landmark predictor and the face aligner
+	# detector = dlib.get_frontal_face_detector()
+	# predictor = dlib.shape_predictor(shape_pred)
+	# fa = FaceAligner(predictor, desiredFaceWidth=152)
 
-	# show the original input image and detect faces in the grayscale
-	# image
-	# cv2.imshow("Input", image)
-	rects = detector(gray, 2)
-	numImg=1
-	# loop over the face detections
-	for rect in rects:
-		# extract the ROI-region of interest of the *original* face, then align the face
-		# using facial landmarks
-		(x, y, w, h) = rect_to_bb(rect)
-		faceOrig = imutils.resize(image[y:y + h, x:x + w], width=152)
-		faceAligned = fa.align(image, gray, rect)
+	# # show the original input image and detect faces in the grayscale
+	# # image
+	# # cv2.imshow("Input", image)
+	# rects = detector(gray, 2)
+	# numImg=1
+	# # loop over the face detections
+	# for rect in rects:
+	# 	# extract the ROI-region of interest of the *original* face, then align the face
+	# 	# using facial landmarks
+	# 	(x, y, w, h) = rect_to_bb(rect)
+	# 	faceOrig = imutils.resize(image[y:y + h, x:x + w], width=152)
+	# 	faceAligned = fa.align(image, gray, rect)
 		
-		#save aligned & original images- for comparison
-		cv2.imwrite(dest_folder+"aligned"+str(numImg)+filename, faceAligned)
+	# 	#save aligned & original images- for comparison
+	# 	cv2.imwrite(dest_folder+"/"+str(numImg)+filename, faceAligned)
 		
-		shape = predictor(gray, rect)
-		shape = face_utils.shape_to_np(shape)
-		cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-		# loop over the (x, y)-coordinates for the facial landmarks
-		# and draw them on the image
-		for (x, y) in shape:
-			cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
+	# 	shape = predictor(gray, rect)
+	# 	shape = face_utils.shape_to_np(shape)
+	# 	cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+	# 	# loop over the (x, y)-coordinates for the facial landmarks
+	# 	# and draw them on the image
+	# 	for (x, y) in shape:
+	# 		cv2.circle(image, (x, y), 1, (0, 0, 255), -1)
 
-		numImg=numImg+1
+	# 	numImg=numImg+1
+
+
+	# The 1 in the second argument indicates that we should upsample the image
+	# 1 time.  This will make everything bigger and allow us to detect more
+	# faces.
+	dets = cnn_face_detector(image, 1)
+	'''
+	This detector returns a mmod_rectangles object. This object contains a list of mmod_rectangle objects.
+	These objects can be accessed by simply iterating over the mmod_rectangles object
+	The mmod_rectangle object has two member variables, a dlib.rectangle object, and a confidence score.
+
+	It is also possible to pass a list of images to the detector.
+	- like this: dets = cnn_face_detector([image list], upsample_num, batch_size = 128)
+
+	In this case it will return a mmod_rectangless object.
+	This object behaves just like a list of lists and can be iterated over.
+	'''
+	print("Number of faces detected: {}".format(len(dets)))
+	for i, d in enumerate(dets):
+		print("Detection {}: Left: {} Top: {} Right: {} Bottom: {} Confidence: {}".format(i, d.rect.left(), d.rect.top(), d.rect.right(), d.rect.bottom(), d.confidence))
+
+	rects = dlib.rectangles()
+	rects.extend([d.rect for d in dets])
+
+	win.clear_overlay()
+	win.set_image(image)
+	win.add_overlay(rects)
+	dlib.hit_enter_to_continue()
+
 
 	# # save the output image with the face detections + facial landmarks
 	cv2.imwrite("/home/das/opencv/samples/Das/Das_pre_trained/results_das/res_" + filename,image) 
--- face_rec_pre.py
+++ face_rec_pre.py
@@ -30,7 +30,9 @@ for filename in os.listdir(source_folder):
 		known_faces=np.concatenate((known_faces, [face_det_encoding]), axis=0)
 		numImgs=numImgs+1
 
-dest_folder="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces"
+date = "19-10-17"
+
+dest_folder="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces"+ "_" +date
 
 # file_1="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces_3d/al_08.png"
 # unknown_face = face_recognition.load_image_file(file_1)
@@ -46,6 +48,9 @@ dest_folder="/home/das/opencv/samples/Das/Das_pre_trained/aligned_faces"
 workbook = xlsxwriter.Workbook('results_recog1.xlsx')
 worksheet = workbook.add_worksheet()
 
+workbook2 = xlsxwriter.Workbook('attendance.xlsx')
+worksheet2 = workbook2.add_worksheet()
+
 results={}
 num=2
 
@@ -59,6 +64,40 @@ worksheet.write('A1', 'FileName')
 worksheet.write('B1', 'similarity measure')
 worksheet.write('C1', 'Similar Training Image')
 worksheet.write('F1', 'Test Image')
+
+worksheet2.set_column('A:A', 20)
+worksheet2.set_column('B:B', 10)
+# worksheet2.set_column('C:C', 10)
+
+worksheet2.write('A1', 'Roll No.s')
+worksheet2.write('B1', date)
+# worksheet2.write('C1', 'PIC')
+
+roll_nos=[]
+for idx in map_label_name:
+	roll_nos.append(map_label_name[idx][:-4])
+roll_nos.sort()
+
+nm=2
+map_roll_no_nm = {}
+for elem in roll_nos:
+
+	worksheet2.write('A{:d}'.format(nm), elem)
+	worksheet2.write('B{:d}'.format(nm), 'A')
+
+
+	# img1=mpimg.imread(os.path.join(source_folder,elem+'.JPG'))
+	# shp=img1.shape
+	# image_width = shp[0]
+	# image_height = shp[1]
+	# cell_width = 80.0
+	# cell_height = 60.0
+	# x_scale = cell_width/image_width * 1/10
+	# y_scale = cell_height/image_height * 1/10
+	# worksheet2.insert_image('C{:d}'.format(nm), os.path.join(source_folder,elem+'.JPG'),{'x_scale': x_scale, 'y_scale': y_scale})
+	
+	map_roll_no_nm[elem] = nm
+	nm=nm+1
 			
 for filename in os.listdir(dest_folder):
 	unknown_face = face_recognition.load_image_file(os.path.join(dest_folder,filename))
@@ -69,9 +108,11 @@ for filename in os.listdir(dest_folder):
 		# results is an array of True/False telling if the unknown face matched anyone in the known_faces array
 		results[filename] = face_recognition.compare_faces(known_faces, unknown_face_encoding)
 		face_dist =face_recognition.face_distance(known_faces, unknown_face_encoding)
+		max_face_dist = 10000
+
 		for idx, val in enumerate(results[filename]):
 			if val==True:
-				print(filename, map_label_name[idx])
+				# print(filename, map_label_name[idx])
 				img1=mpimg.imread(os.path.join(source_folder,map_label_name[idx]))
 				img2=mpimg.imread(os.path.join(dest_folder,filename))
 				
@@ -83,8 +124,12 @@ for filename in os.listdir(dest_folder):
 				x_scale = cell_width/image_width
 				y_scale = cell_height/image_height
 
+				if face_dist[idx] < max_face_dist:
+					max_face_dist = face_dist[idx]
+					max_face_dist_label = map_label_name[idx][:-4]
+
 				# Insert an image.
-				worksheet.write('A{:d}'.format(num), map_label_name[idx])
+				worksheet.write('A{:d}'.format(num),  map_label_name[idx][:-4])
 				worksheet.write('B{:d}'.format(num), face_dist[idx])
 				worksheet.insert_image('C{:d}'.format(num), os.path.join(source_folder,map_label_name[idx]),{'x_scale': x_scale, 'y_scale': y_scale})
 
@@ -98,6 +143,12 @@ for filename in os.listdir(dest_folder):
 				worksheet.insert_image('F{:d}'.format(num), os.path.join(dest_folder,filename),{'x_scale': x_scale, 'y_scale': y_scale})
 				num=num+15
 
+		try:
+			worksheet2.write('B{:d}'.format(map_roll_no_nm[max_face_dist_label]), 'P')
+		except:
+			pass
+
+workbook2.close()
 workbook.close()
 
 
